9e57bcc6e38e523b0f35d61f7b581bf2ea63ac60
wanya2002 <wanya2002@yandex.ru>
2023-04-18 21:42:55 +0300

    new commit

---
+++ tests/test_utils.py
@@ -0,0 +1,5 @@
+from utils import sort_data
+
+def test_sort_data(test_data):
+    sorted_data = sort_data(test_data)
+    assert [x['date'] for x in sorted_data] == ['2019-08-26T10:50:58.294041', '2019-07-03T18:35:29.512364', '2018-06-30T02:08:58.425572']
---
+++ utils.py
@@ -0,0 +1,17 @@
+import json
+
+def get_data():
+   with open('oprations.json', 'r', encoding="utf-8") as file:
+       data = json.load(file)
+   return data
+
+def filter_data(data):
+    data_filtr = []
+    for el in data:
+        if 'state' in el and el['state'] == 'EXECUTED':
+            data_filtr.append(el)
+    return data_filtr
+
+def sort_data(data):
+    data = sorted(data, key = lambda x: x['date'], reverse=True)
+    return data[:5]
